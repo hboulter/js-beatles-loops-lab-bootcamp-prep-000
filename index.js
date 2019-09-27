083f6d976a90a13048868d9fbfa156e8bedb58d0
Heidi B <boulter.heidi@gmail.com>
2019-09-27 18:18:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-for (var i = 0; i <= musicians.length; i++) {
-  var x = [];
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i <= musicians.length; i++) {
+    var x = [];
     x.push(`${musicians} plays ${instruments}`)
   } return x
 }
